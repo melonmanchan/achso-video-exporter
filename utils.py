0a857272a2ae5396a0081a2785fe379c0488edb0
Matti Jokitulppo <melonmanchan@gmail.com>
2016-05-13 10:50:08 +0300

    Add functional file downloading

--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
-
-from requests import  request
+import requests
 
 def download_file(url, path):
     print(url)
     print(path)
+
+    r = requests.get(url, stream=True)
+    with open("video-cache/" + path, "wb") as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
