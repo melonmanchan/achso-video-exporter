3273a5684b72bed9ee4b79c8611f5693aae65b97
Matti Jokitulppo <melonmanchan@gmail.com>
2016-05-16 16:28:07 +0300

    Semi-functional annotation pause generation

--- videoeditor.py
+++ videoeditor.py
@@ -20,23 +20,20 @@ def generate_pauses(video_clip, annotations):
     """Takes in a regular video clip, and bakes in annotation pauses"""
     pause_time = 0.5
     clip_duration = video_clip.duration + (len(annotations) * pause_time)
-    last_annotation_time = 0
-    video_pause_frame_count = int(video_clip.fps * pause_time)
-    video_clips = []
+    video_clips = [ video_clip ]
     for annotation in annotations:
-
-        annotation_frames = []
         current_annotation_time = annotation["time"] / 1000.0
-        video_clips.append(video_clip.subclip(t_start=last_annotation_time, t_end=last_annotation_time + pause_time))
         pause_frame = video_clip.to_ImageClip(current_annotation_time)
-        pause_frame.set_duration(pause_time)
 
-        #for _ in range(video_pause_frame_count):
-            #annotation_frames.append(pause_frame)
+        print("starting annotation at " + str(current_annotation_time))
+        print("ending annotation at " + str(current_annotation_time + pause_time))
 
-        # video_clips.append(ImageSequenceClip(sequence=[pause_frame], durations=[pause_time]))
+        pause_frame = pause_frame.set_start(current_annotation_time)
+        pause_frame = pause_frame.set_end(current_annotation_time + pause_time)
+        print(pause_frame.start)
+        print(pause_frame.end)
+        print(pause_frame.duration)
+
         video_clips.append(pause_frame)
 
-        last_annotation_time = current_annotation_time
-
-    return concatenate_videoclips(video_clips)
+    return CompositeVideoClip(video_clips).set_duration(clip_duration)
