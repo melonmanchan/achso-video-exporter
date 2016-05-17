77331267fa193f383675d58197ff9ea31c2e487b
Matti Jokitulppo <melonmanchan@gmail.com>
2016-05-17 12:06:30 +0300

    Allow incoming JSON as both object and array of objects

--- main.py
+++ main.py
@@ -11,13 +11,18 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
 def index():
-    content = request.get_json()
-    video_uri = content["videoUri"]
-    video_filename = video_uri.rsplit("/")[-1]
+    request_json = request.get_json()
 
-    video_location = download_file(video_uri, video_filename)
-    sorted_annotations = sort_annotations_by_time(content["annotations"])
-    bake_annotations(video_location, video_filename, sorted_annotations)
+    # Allow both plain JSON objects and arrays
+    if type(request_json) is dict:
+        request_json = [request_json]
+
+    for annotation_json in request_json:
+        video_uri = annotation_json["videoUri"]
+        video_filename = video_uri.rsplit("/")[-1]
+        video_location = download_file(video_uri, video_filename)
+        sorted_annotations = sort_annotations_by_time(annotation_json["annotations"])
+        bake_annotations(video_location, video_filename, sorted_annotations)
 
     return jsonify({"message": "Annotated video created succesfully"})
 
