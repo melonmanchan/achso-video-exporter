85cecfb1d0f58274b21d6765f13240323770840c
Matti Jokitulppo <melonmanchan@gmail.com>
2016-05-18 15:15:19 +0300

    Add annotation markers

--- annotations.py
+++ annotations.py
@@ -1,4 +1,6 @@
+from moviepy.editor import *
 
+marker_image = ImageClip("AS_annotation.png")
 
 def get_annotations_added_duration(annotations):
     duration = 0
@@ -13,3 +15,29 @@ def get_annotation_duration(annotation):
 
 def sort_annotations_by_time(annotations):
     return sorted(annotations, key=lambda k: k["time"])
+
+
+def get_marker_absolute_pos(marker_position, clip):
+    marker_x = marker_position["x"] * clip.w
+    marker_y = marker_position["y"] * clip.h
+    return marker_x, marker_y
+
+
+def get_subtitle(annotation, sub_duration):
+    txt_clip = TextClip(annotation["text"], color="white", fontsize=70)
+    txt_clip = txt_clip.set_position(("center", "bottom"))
+    txt_clip = txt_clip.set_start(float(annotation["time"]) / 1000.0)
+    txt_clip = txt_clip.set_duration(sub_duration)
+
+    return txt_clip
+
+
+def get_marker(annotation, marker_duration, video_clip):
+    position = annotation["position"]
+    marker = marker_image.copy()
+    marker = marker.set_position(get_marker_absolute_pos(position, video_clip))
+    marker = marker.set_start(float(annotation["time"]) / 1000.0)
+    marker = marker.set_duration(marker_duration)
+
+    return marker
+
--- videoeditor.py
+++ videoeditor.py
@@ -1,24 +1,25 @@
 from moviepy.editor import *
-from annotations import get_annotation_duration
+from annotations import get_annotation_duration, get_subtitle, get_marker
 
+
+marker_image = ImageClip("AS_annotation.png")
 
 def bake_annotations(video_file, end_point, annotations):
     clip = VideoFileClip(video_file)
-    subtitled_video = generate_subtitles(clip, annotations)
-    paused_video = generate_pauses(subtitled_video, annotations)
+    annotated_video = generate_annotation_markings(clip, annotations)
+    paused_video = generate_pauses(annotated_video, annotations)
     paused_video.write_videofile("video-out/" + end_point)
 
 
-def generate_subtitles(video_clip, annotations):
+def generate_annotation_markings(video_clip, annotations):
     composite_clips = [video_clip]
     one_frame_time = 1 / video_clip.fps
 
     for annotation in annotations:
-        txt_clip = TextClip(annotation["text"], color="white", fontsize=70)
-        txt_clip = txt_clip.set_position(("center", "bottom"))
-        txt_clip = txt_clip.set_start(float(annotation["time"]) / 1000.0)
-        txt_clip = txt_clip.set_duration(one_frame_time)
+        txt_clip = get_subtitle(annotation, one_frame_time)
+        marker = get_marker(annotation, one_frame_time, video_clip)
         composite_clips.append(txt_clip)
+        composite_clips.append(marker)
 
     return CompositeVideoClip(composite_clips)
 
@@ -31,3 +32,5 @@ def generate_pauses(video_clip, annotations):
         video_clip = video_clip.fx(vfx.freeze, t=current_annotation_time, freeze_duration=pause_time)
 
     return video_clip
+
+
