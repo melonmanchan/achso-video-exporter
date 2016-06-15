e5662272bb3d0c3a197c37212afd2390af9ec8de
Matti Jokitulppo <melonmanchan@gmail.com>
2016-06-15 13:30:21 +0300

    Add all exported videos to single .zip

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 
+import uuid
+
 from utils import download_file, create_temp_dir, zip_up_dir, delete_dir, is_video_json_valid
 from annotations import sort_annotations_by_time, is_annotation_json_valid
 
@@ -20,6 +22,7 @@ def index():
     if type(request_json) is dict:
         request_json = [request_json]
 
+    export_dir_name = create_temp_dir()
     for video_json in request_json:
         if not is_video_json_valid(video_json):
             return jsonify({"message": "A video json was malformed"}), 400
@@ -29,17 +32,14 @@ def index():
         if not is_annotation_json_valid(video_json["annotations"]):
             return jsonify({"message": "An annotation json was malformed"}), 400
         else:
-            export_dir_name = create_temp_dir()
-
             video_uri = video_json["videoUri"]
             video_filename = video_uri.rsplit("/")[-1]
             video_location = download_file(video_uri, "video-cache/" + video_filename)
             sorted_annotations = sort_annotations_by_time(video_json["annotations"])
-            bake_annotations(video_location, export_dir_name + "/" + video_filename, sorted_annotations)
+            bake_annotations(video_location, export_dir_name + "/" + video_json["title"] + ".mp4", sorted_annotations)
 
-            zip_up_dir(export_dir_name, 'video-exports/' + video_json["title"])
-            delete_dir(export_dir_name)
-
+    zip_up_dir(export_dir_name, 'video-exports/AchSo-Video-Export' + str(uuid.uuid4()))
+    delete_dir(export_dir_name)
     return jsonify({"message": "Annotated video created successfully"})
 
 if __name__ == "__main__":
