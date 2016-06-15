14b7cb27e0a5ac4bf6e65f87ea1c3027af4350ee
Matti Jokitulppo <melonmanchan@gmail.com>
2016-06-15 18:00:38 +0300

    Bind flask app to 0.0.0.0

--- main.py
+++ main.py
@@ -43,4 +43,4 @@ def index():
     return jsonify({"message": "Annotated video created successfully"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
