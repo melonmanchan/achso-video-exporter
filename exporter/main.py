65ff9f846d1862b11f5d71671bc3b65481e86448
Matti Jokitulppo <melonmanchan@gmail.com>
2016-06-27 14:33:10 +0300

    Uncomment commented out test code

--- exporter/main.py
+++ exporter/main.py
@@ -23,7 +23,7 @@ celery = make_celery(app)
 
 @celery.task(name='exporter.tasks.export_videos')
 def export_videos():
-    return 1 + 2
+    print("HELLO")
 
 
 @app.route("/", methods=["POST"])
@@ -31,48 +31,46 @@ def index():
     request_json = request.get_json()
 
     result = export_videos.delay()
-    result.wait()
-    return jsonify({"result": result.result})
-    #if "email" not in request_json:
-    #    return jsonify({"message": "Email address of recipient was missing!"}), 400
+    if "email" not in request_json:
+        return jsonify({"message": "Email address of recipient was missing!"}), 400
 
-    #if "videos" not in request_json:
-    #    return jsonify({"message": "Videos to export were missing!"}), 400
+    if "videos" not in request_json:
+        return jsonify({"message": "Videos to export were missing!"}), 400
 
-    #email = request_json["email"]
-    #videos = request_json["videos"]
+    email = request_json["email"]
+    videos = request_json["videos"]
 
-    ## Allow both plain JSON objects and arrays
-    #if type(videos) is dict:
-    #    videos = [videos]
+    # Allow both plain JSON objects and arrays
+    if type(videos) is dict:
+        videos = [videos]
 
-    #export_dir_name = create_temp_dir()
-    #for video_json in videos:
-    #    if not is_video_json_valid(video_json):
-    #        return jsonify({"message": "A video json was malformed"}), 400
-    #    elif "annotations" not in video_json or not video_json["annotations"]:
-    #        download_file(video_json["videoUri"], export_dir_name + "/" + video_json["title"] + ".mp4")
-    #        break
-    #    elif not is_annotation_json_valid(video_json["annotations"]):
-    #        return jsonify({"message": "An annotation json was malformed"}), 400
-    #    else:
-    #        video_uri = video_json["videoUri"]
-    #        video_filename = video_uri.rsplit("/")[-1]
-    #        video_location = download_file(video_uri, "../video-cache/" + video_filename)
-    #        sorted_annotations = sort_annotations_by_time(video_json["annotations"])
-    #        bake_annotations(video_location, export_dir_name + "/" + video_json["title"] + ".mp4", sorted_annotations)
-    #        delete_file("../video-cache/" + video_filename)
+    export_dir_name = create_temp_dir()
+    for video_json in videos:
+        if not is_video_json_valid(video_json):
+            return jsonify({"message": "A video json was malformed"}), 400
+        elif "annotations" not in video_json or not video_json["annotations"]:
+            download_file(video_json["videoUri"], export_dir_name + "/" + video_json["title"] + ".mp4")
+            break
+        elif not is_annotation_json_valid(video_json["annotations"]):
+            return jsonify({"message": "An annotation json was malformed"}), 400
+        else:
+            video_uri = video_json["videoUri"]
+            video_filename = video_uri.rsplit("/")[-1]
+            video_location = download_file(video_uri, "../video-cache/" + video_filename)
+            sorted_annotations = sort_annotations_by_time(video_json["annotations"])
+            bake_annotations(video_location, export_dir_name + "/" + video_json["title"] + ".mp4", sorted_annotations)
+            delete_file("../video-cache/" + video_filename)
 
-    #export_zip_name = '../video-exports/AchSo-Video-Export-' + str(uuid.uuid4())
-    #export_zip_name = zip_up_dir(export_dir_name, export_zip_name)
-    #delete_dir(export_dir_name)
+    export_zip_name = '../video-exports/AchSo-Video-Export-' + str(uuid.uuid4())
+    export_zip_name = zip_up_dir(export_dir_name, export_zip_name)
+    delete_dir(export_dir_name)
 
-    #response, url = upload_file(export_zip_name)
-    #delete_file(export_zip_name)
+    response, url = upload_file(export_zip_name)
+    delete_file(export_zip_name)
 
-    #send_download_link(email, url)
+    send_download_link(email, url)
 
-    # return jsonify({"message": "Annotated video created successfully", "url": url})
+    return jsonify({"message": "Annotated video created successfully", "url": url})
 
 if __name__ == "__main__":
     app.run(debug=config.DEBUG, host=config.HOST, port=config.PORT)
