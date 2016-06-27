1bbe01fb9cafcb2effd6e95f40ae5c9872469f08
Matti Jokitulppo <melonmanchan@gmail.com>
2016-06-27 13:11:31 +0300

    Add heading to export mail

--- exporter/mailer.py
+++ exporter/mailer.py
@@ -7,7 +7,7 @@ from_mail = sendgrid.Email(config.SENDGRID_FROM_MAIL)
 
 def send_download_link(to, link):
     to_mail = sendgrid.Email(to)
-    content = Content("text/html", "<html> Your download link is <a href='{0}'>{1}</a> </html>".format(link, link))
+    content = Content("text/html", "<html> <h1>Your videos are ready</h1> Hello! Your download link is <a href='{0}'>{1}</a> </html>".format(link, link))
     message = Mail(from_email=from_mail, subject='Your AchSo! video export is ready',
                    to_email=to_mail, content=content)
     resp = sg.client.mail.send.post(request_body=message.get())
