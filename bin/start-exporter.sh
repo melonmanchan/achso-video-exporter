75845dfe16b77e94af51d3c3ffdbe4265e6ed8f5
Matti Jokitulppo <melonmanchan@gmail.com>
2016-06-27 16:30:34 +0300

    Restart redis server on start-up

--- bin/start-exporter.sh
+++ bin/start-exporter.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-service supervisor stop
-service supervisor start
+service redis-server restart
+service supervisor restart
 
 supervisorctl start celery
 python /achso-video-exporter/exporter/main.py
