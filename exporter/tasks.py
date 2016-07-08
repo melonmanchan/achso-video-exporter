a51c2a62344c090c0d006abaa9a6a0a881a770b4
Matti Jokitulppo <melonmanchan@gmail.com>
2016-07-08 15:33:30 +0300

    Document tasks.py

--- exporter/tasks.py
+++ exporter/tasks.py
@@ -3,6 +3,15 @@ import config as config
 
 
 def make_celery(app):
+    """
+    Simple helper function to tie together the Flask and Celery parts of the exporter
+
+    Args:
+        app (Flask.App): The Flask application object
+
+    Returns:
+        Celery: The celery application
+    """
     celery = Celery(app.import_name, backend=config.CELERY_RESULT_BACKEND, broker=config.CELERY_BROKER_URL)
     celery.conf.update(CELERY_ACCEPT_CONTENT=['json'], CELERY_TASK_SERIALIZER='json', CELERY_RESULT_SERIALIZER='json')
 
